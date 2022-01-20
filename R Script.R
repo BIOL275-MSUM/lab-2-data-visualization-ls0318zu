c2aaf1d6f95b2d2f50437d340f6e33791affa50d
Kameron Knoll <ls0318zu@mnstate.edu>
2022-01-20 13:11:10 -0600

    Added code to my R script.

--- R Script.R
+++ R Script.R
@@ -1,4 +1,22 @@
-ggplot(data = mpg) + 
-  geom_point(mapping = aes(x = displ, y = hwy))
-penguins
-ggpolt(data=penguins)
+# load packages
+library(tidyverse)
+library(palmerpenguins)
+
+# 10 b
+ggplot(data = penguins)
+
+# 10 c
+ggplot(data = penguins) + 
+  geom_point(mapping=aes(x=body_mass_g,y=flipper_length_mm))
+
+#_ 11 a
+ggplot(data = penguins) + 
+  geom_point(mapping=aes(x=body_mass_g,y=flipper_length_mm,color=species))
+#_ 11 b
+ggplot(data = penguins) + 
+  geom_point(mapping=aes(x=body_mass_g,y=flipper_length_mm,color=species,shape=island))
+#_ 13 a and b
+ggplot(data = penguins) + 
+  geom_point(mapping=aes(x=body_mass_g,y=flipper_length_mm,color=species,shape=island))+
+  facet_wrap(~species)
+#_ 13 c
